47174061049bee11c126643bea9449a3ebe029f8
HTL2910 <hathanhlong2910@gmail.com>
2023-09-29 11:53:37 +0700

    update ep3

---
+++ Assets/Scripts/InteractableObject.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractableObject : MonoBehaviour
+{
+    public string itemName;
+
+    public string GetItemName(){ return itemName; }
+}
--- Assets/Scripts/SelectionManager.cs
+++ Assets/Scripts/SelectionManager.cs
@@ -1,18 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
+using TMPro;
 public class SelectionManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public GameObject interaction_info_UI;
+    private TextMeshProUGUI interactionText;
 
-    // Update is called once per frame
-    void Update()
+    private void Start()
     {
-        
+        interactionText = interaction_info_UI.GetComponent<TextMeshProUGUI>();
+        interaction_info_UI.gameObject.SetActive(false);
+    }
+    private void Update()
+    {
+        Ray ray=Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        if(Physics.Raycast(ray, out hit) )
+        {
+            var selectionTransform = hit.transform;
+            if (selectionTransform.GetComponent<InteractableObject>())
+            {
+                interactionText.text= selectionTransform.GetComponent<InteractableObject>().GetItemName();
+                interaction_info_UI.gameObject.SetActive(true);
+            }
+            else
+            {
+                interaction_info_UI.gameObject.SetActive(false);
+            }
+        }    
     }
 }
