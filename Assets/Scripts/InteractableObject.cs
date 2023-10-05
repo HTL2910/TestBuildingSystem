cf4b912dbe0212d9c48d9f4a8f40c7c51f5a3b7b
HTL2910 <hathanhlong2910@gmail.com>
2023-10-05 17:29:00 +0700

    add asset rock

--- Assets/Scripts/InteractableObject.cs
+++ Assets/Scripts/InteractableObject.cs
@@ -5,6 +5,20 @@ using UnityEngine;
 public class InteractableObject : MonoBehaviour
 {
     public string itemName;
-
+    public bool playerInRange;
     public string GetItemName(){ return itemName; }
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Player"))
+        {
+            playerInRange = true;
+        }    
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerInRange = false;
+        }
+    }
 }
